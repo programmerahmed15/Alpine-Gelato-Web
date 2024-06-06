f92d2ec728e9d0839706aeab4de800d6736499f0
programmerahmed15 git config --global user.email ahmedprogrammer913@gmail.com <you@example.com>
2024-06-06 18:59:03 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
